be40f4c8e7fba7e5e7f13963702505551611dafc
Antonio Felix <39065830+TheCoolAntonioFelix@users.noreply.github.com>
2018-05-21 10:45:34 -0700

    Update SimpGraphics.java

--- SimpGraphics.java
+++ SimpGraphics.java
@@ -8,20 +8,19 @@ public interface SimpGraphics {
 	 * additional methods we might require.
 	 */
 
-	public boolean isActive(); // Checks whether the implemented class's variable was
-							   // evoked/activated. If the variable is false then return
-							   // false
+	public boolean isActive();  // Checks whether the implemented class's variable was
+				   // evoked/activated. If the variable is false then return false
 
-	public void inactiveTimer(int seconds); // if a user hasn't pressed a button or touched the screen
-											// in a set amount of time then set your chosen
-											// variable as false
+	public void inactiveTimer(int seconds);  // if a user hasn't pressed a button or touched the screen
+					        // in a set amount of time then set your chosen
+					       // variable as false
 
 	public void create(); // Constructs JFrame for your group
 
 	public void destroy(); // Turns off JFrame
 
-	public void rescale(double scale); // This replaces the enlarge and shrink methods. A
-										// scale factor will be passed as a parameter. A
-										// scale of 2 should make your components twice as
-										// large while a 0.5 scale makes it half as large.
+	public void rescale(double scale);   // This replaces the enlarge and shrink methods. A
+					    // scale factor will be passed as a parameter. A
+				           // scale of 2 should make your components twice as
+					  // large while a 0.5 scale makes it half as large.
 }
