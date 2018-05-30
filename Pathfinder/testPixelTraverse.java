b503ffe1b6aa0f83ef2d014e2897741c92952158
PrinceB3N <39066183+PrinceB3N@users.noreply.github.com>
2018-05-30 14:36:08 -0700

    Update testPixelTraverse.java

--- Pathfinder/testPixelTraverse.java
+++ Pathfinder/testPixelTraverse.java
@@ -1,6 +1,8 @@
 package Pathfinder;
 
 import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.Image;
 import java.awt.Point;
 import java.awt.image.BufferedImage;
 import java.awt.image.ColorModel;
@@ -14,29 +16,39 @@ import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-
 public class testPixelTraverse {
 
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
-		BufferedImage b = ImageIO.read(new File("thresh.jpg"));
-		
-		PixelTraverse pt = new PixelTraverse(b,16777216 ,50, 50, 100,100);
+		BufferedImage b = ImageIO.read(new File("40133654-422f3c42-58f5-11e8-92ff-88f9042b3984.png"));
+
+
+		//The Rock:(b.getWidth()/3+80, 80)
+		//Turner:672, 840
+		PixelTraverse pt = new PixelTraverse(b,1,672,840,468,80);
+		//16777216
+		//OR: 1
 		ArrayList<Point> sol =pt.startpixeltraverse();
-		BufferedImage img= deepCopy(b);
+	//	BufferedImage img= deepCopy(b);
 		for(Point pp:sol) {
-			img.setRGB((int)pp.getY(), (int)pp.getX(),Color.YELLOW.getRGB());
-		}
+			System.out.println(pp.getX()+", "+pp.getY());
+		
+			
+			
+			b.setRGB((int)pp.getX(),(int)pp.getY(),Color.RED.getRGB());
+			
+			}
 		
 		JFrame j = new JFrame();
 		j.setSize(b.getWidth(), b.getHeight());
 		JPanel jp = new JPanel();
-		JLabel jl = new JLabel(new ImageIcon(img));
+	//	JLabel jl = new JLabel(new ImageIcon(img));
 		JLabel jorigin=new JLabel(new ImageIcon(b));
 		j.add(jp);
 		jp.add(jorigin);
-		jp.add(jl);
+	//	jp.add(jl);
 		j.setVisible(true);
+		j.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
 		
 	}
@@ -46,4 +58,12 @@ public class testPixelTraverse {
 		 WritableRaster raster = bi.copyData(null);
 		 return new BufferedImage(cm, raster, isAlphaPremultiplied, null);
 		}
+	private static BufferedImage resize(BufferedImage img, int height, int width) {
+        Image tmp = img.getScaledInstance(width, height, Image.SCALE_SMOOTH);
+        BufferedImage resized = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
+        Graphics2D g2d = resized.createGraphics();
+        g2d.drawImage(tmp, 0, 0, null);
+        g2d.dispose();
+        return resized;
+}
 }
